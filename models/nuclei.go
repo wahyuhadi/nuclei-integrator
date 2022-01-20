2474c38377ede317e5febac503b122574ee007f1
wahyuhadi <hrahmatwahyu@gmail.com>
2022-01-20 22:45:00 +0800

    initial

--- models/nuclei.go
+++ models/nuclei.go
@@ -3,20 +3,31 @@ package models
 import "time"
 
 type Nuclei struct {
-	TemplateID *string `json:"template-id"`
-	Info       struct {
-		Name        *string      `json:"name"`
-		Author      *[]string    `json:"author"`
-		Tags        *[]string    `json:"tags"`
-		Description *string      `json:"description"`
-		Reference   *interface{} `json:"reference"`
-		Severity    *string      `json:"severity"`
+	Template    *string `json:"template"`
+	TemplateURL *string `json:"template-url"`
+	TemplateID  string  `json:"template-id"`
+	Info        struct {
+		Name           *string   `json:"name"`
+		Author         *[]string `json:"author"`
+		Tags           *[]string `json:"tags"`
+		Description    *string   `json:"description"`
+		Reference      *[]string `json:"reference"`
+		Severity       *string   `json:"severity"`
+		Classification struct {
+			CveID       *[]string `json:"cve-id"`
+			CweID       *[]string `json:"cwe-id"`
+			CvssMetrics *string   `json:"cvss-metrics"`
+			CvssScore   *float64  `json:"cvss-score"`
+		} `json:"classification"`
 	} `json:"info"`
-	Type          *string    `json:"type"`
-	Host          *string    `json:"host"`
-	MatchedAt     *string    `json:"matched-at"`
+	Type             *string   `json:"type"`
+	Host             *string   `json:"host"`
+	MatchedAt        *string   `json:"matched-at"`
+	ExtractedResults *[]string `json:"extracted-results"`
+	Meta             struct {
+		Hostname *string `json:"Hostname"`
+	} `json:"meta"`
 	IP            *string    `json:"ip"`
 	Timestamp     *time.Time `json:"timestamp"`
-	CurlCommand   *string    `json:"curl-command"`
 	MatcherStatus *bool      `json:"matcher-status"`
 }
